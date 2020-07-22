4fd4c0af2ae5598cf16d3718985cedc8d8295d24
Shravan <heyshrav@gmail.com>
2020-07-22 21:13:02 +0530

    build fix try 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./Home";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
