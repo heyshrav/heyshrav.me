877de89b00f477188ac4a62c5b2eea652905ac07
Shravan <heyshrav@gmail.com>
2020-07-22 21:00:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Home from "./Homepage";
+import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
