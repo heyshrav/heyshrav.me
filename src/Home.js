858508a6a4a452ea1f3d7846e5afbe097ac0af0f
Shravan <heyshrav@gmail.com>
2021-03-27 12:14:15 +0530

    new section

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Home() {
+
   return (
     <div>
 
@@ -16,6 +17,8 @@ function Home() {
               <div class="navbar-nav ">
                 <a class="nav-link" aria-current="page" href="/"> <span><i class="fas fa-paper-plane"></i></span>reach@heyshrav.me</a>
               </div>
+
+
             </div>
           </div>
         </nav>
@@ -46,11 +49,87 @@ function Home() {
 
       <section id="section2">
         <div className="container">
-          <h2 className="mx-auto">Howdy?</h2>
+          <div className="row">
+            <div className="col d-flex justify-content-center">
+              <div className="card sub_section2">
+                <div>
+                  <h3 className="pb-1">About</h3>
+                </div>
+                <div>
+                  <p className="text-muted text-break">I am a UI/UX Designer and Front-end Developer. I have been doing freelance Graphic Designing from past 2 years and also been working on web designing.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col d-flex justify-content-center">
+              <div className="card sub_section2">
+                <div>
+                  <h3 className="pb-1">Education</h3>
+                </div>
+                <div className="education_sec">
+                  <div>
+                    <h5>Global Academy of Technology</h5><span>(2017-2021)</span>
+                  </div>
+                  <div>
+                    Bachelor's degree, Computer Science
+                  </div>
+                  <hr className="w-50" />
+                </div>
+                <div className="education_sec">
+                  <div>
+                    <h5>Maharishi Vidya Mandir, Noida</h5><span>(2004-2017)</span>
+                  </div>
+                  <div>
+                    School, PCM + Information Practices
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div className="col d-flex justify-content-center">
+              <div className="card sub_section2">
+                <div>
+                  <h3 className="pb-1">Skills</h3>
+                </div>
+
+                <div className="skill_sec">
+                  <span> <i class="fab fa-html5    "></i> HTML5</span>
+                  <div class="progress">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+                <div className="skill_sec">
+                  <span> <i class="fab fa-css3" aria-hidden="true"></i> CSS3 </span>
+                  <div class="progress">
+
+                    <div class="progress-bar progress-bar-striped progress-bar-animated w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+                <div className="skill_sec">
+                  <span>Bootstrap</span>
+                  <div class="progress">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+                <div className="skill_sec">
+                  <span> <i class="fab fa-react"></i> React</span>
+                  <div class="progress">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated w-50" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
         </div>
       </section>
 
-    </div>
+      <section id="section3">
+        <div className="container">
+          #section3
+        </div>
+      </section>
+    </div >
   );
 }
 
